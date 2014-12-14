65976bd3e636677a7e93733abc86d9916265cbc3
Aleksander Grzyb <aleksander.grzyb@gmail.com>
2014-12-14 19:47:42 +0100

    Initial Commit

---
+++ travelling_salesman_problem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  travelling_salesman_problem
+//
+//  Created by Aleksander Grzyb on 14/12/14.
+//  Copyright (c) 2014 Aleksander Grzyb. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
